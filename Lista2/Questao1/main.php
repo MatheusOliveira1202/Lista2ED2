ca34a23bb75d6205cc9e4db625905bfd212d1741
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 11:46:48 -0300

    Update main.php

--- Lista2/Questao1/main.php
+++ Lista2/Questao1/main.php
@@ -22,5 +22,6 @@ $array = [0, 1, 8, 2, 5, 9];
   }
 
 
+//Explicação : nesse método há duas estruturas lineares, uma dentro da outra, fazendo com que cada iteração do primmeiro conjunto de dados(array), gere uma segunda estrutura linear inteira, assim temos  O(n) x O(n) = O(n²)
 
 ?>
