6976aa118067b048b196d3d1d089d33d10c83051
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 13:13:45 -0300

    Update main.r

--- Lista2/Questao1/main.r
+++ Lista2/Questao1/main.r
@@ -17,3 +17,4 @@ bin_search <- function (v, t) {
 }
 vector2 <- list(c(10,11,12,13,14), c(12))
 do.call(bin_search, vector2)
+#Explicação : aqui temos uma estrutura linear que faz uma leitura de crescente logarítimica, pois sempre estaremnos lendo metade dos dados apresentados e diminuindo os dados pela metade também, assim temos uma estrutura de complexidade O(log n)
