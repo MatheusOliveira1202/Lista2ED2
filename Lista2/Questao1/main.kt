4a6867aea7c5132ad0141544fdf918004f577da6
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 13:18:50 -0300

    Update main.kt

--- Lista2/Questao1/main.kt
+++ Lista2/Questao1/main.kt
@@ -2,3 +2,5 @@ fun main(args: Array<String>) {
   String nome = "fernando";
   println(nome)
 }
+
+//explicação : Aqui temos apenas uma atribuição de um valor à uma variável e a escrita da mesma no console, temos uma ação constante aqui, tornando a complexidade desse código uma O(1)
