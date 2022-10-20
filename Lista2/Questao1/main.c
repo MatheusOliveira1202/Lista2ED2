60e6bc98a35aad1773a023788373a9ca1ca90de7
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 11:24:27 -0300

    Update main.c

--- Lista2/Questao1/main.c
+++ Lista2/Questao1/main.c
@@ -7,3 +7,5 @@ void main()
         printf("Hello World !!!\n");
     }
 }
+
+//explicação : aqui temos uma iteração que cresce duas vezes de tamanho na leitura. É uma estrutura linear pois ainda vai ler um conjunto de dados passado anteriormente e vai crescer de acordo com a leitura, fazendo desse método um método de complexidade O(n)
