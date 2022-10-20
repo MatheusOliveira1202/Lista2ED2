acb60526ab7d2ed4074b9f451e86f40c1296cbc5
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 11:26:00 -0300

    Update main.cpp

--- Lista2/Questao1/main.cpp
+++ Lista2/Questao1/main.cpp
@@ -9,3 +9,5 @@ int main()
     }
     return 0;
 }
+
+//explicação : aqui temos uma iteração que cresce duas vezes de tamanho na leitura. É uma estrutura linear pois ainda vai ler um conjunto de dados passado anteriormente e vai crescer de acordo com a leitura, fazendo desse método um método de complexidade O(n)
