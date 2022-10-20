0b40a0fb3c89b53acae648110f8fa68e153f65fc
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 11:57:10 -0300

    Update main.py

--- Lista2/Questao1/main.py
+++ Lista2/Questao1/main.py
@@ -4,3 +4,5 @@ def quadratic_algo(items):
             print(item, ' ' ,item2)
 
 quadratic_algo([4, 5, 6, 8])
+
+#Explicação : nesse método há duas estruturas lineares, uma dentro da outra, fazendo com que cada iteração do primmeiro conjunto de dados(array), gere uma segunda estrutura linear inteira, assim temos  O(n) x O(n) = O(n²)
