35880772c64d6c29d7bfc4f64d6cffb40359b71d
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 11:28:25 -0300

    Update main.cs

--- Lista2/Questao1/main.cs
+++ Lista2/Questao1/main.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    String nome = "fernando";
+    Console.WriteLine (nome);
   }
 }
+
+//explicação : Aqui temos apenas uma atribuição de um valor à uma variável e a escrita da mesma no console, temos uma ação constante aqui, tornando a complexidade desse código uma O(1)
