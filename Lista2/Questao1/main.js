dcd4a81b02036ea37c4226079848dcbce1c9c541
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 13:33:01 -0300

    Update main.js

--- Lista2/Questao1/main.js
+++ Lista2/Questao1/main.js
@@ -1,2 +1,4 @@
 var numero = 5;
 console.log(numero);
+
+//explicação : Aqui temos apenas uma atribuição de um valor à uma variável e a escrita da mesma no console, temos uma ação constante aqui, tornando a complexidade desse código uma O(1)
