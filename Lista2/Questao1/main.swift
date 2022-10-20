2744491e66a886438f1d463dea18781820a7b5ed
Matheus Oliveira <matolisou@gmail.com>
2022-10-20 12:11:04 -0300

    Update main.swift

--- Lista2/Questao1/main.swift
+++ Lista2/Questao1/main.swift
@@ -1,2 +1,4 @@
 let arrOne = [1, 2, 3, 4, 5, 6]
 print(arrOne[2])
+
+//explicação : Aqui temos apenas uma atribuição de alguns valores à uma variável e a escrita de uma delas no console, temos uma ação constante aqui, tornando a complexidade desse código uma O(1)
